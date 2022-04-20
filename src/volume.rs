a08ec2d0cd2de13544c4f7cd809a822cb0b7b49f
Mikkel-T <mikkel@mikkel-t.com>
2022-04-20 09:51:58 +0200

    Add windows volume control

---
+++ src/volume.rs
@@ -0,0 +1,31 @@
+use windows::core::*;
+use windows::Win32::Media::Audio::Endpoints::*;
+use windows::Win32::Media::Audio::*;
+use windows::Win32::System::Com::*;
+
+fn get_endpoint() -> Result<IAudioEndpointVolume> {
+    unsafe {
+        CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
+        let immde: IMMDeviceEnumerator =
+            CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_ALL)?;
+        let imm = immde.GetDefaultAudioEndpoint(eRender, eMultimedia)?;
+        let mut iae: Option<IAudioEndpointVolume> = None;
+        imm.Activate(
+            &IAudioEndpointVolume::IID,
+            CLSCTX_INPROC_SERVER,
+            core::ptr::null(),
+            &mut iae as *mut _ as *mut _,
+        )?;
+
+        Ok(iae.unwrap())
+    }
+}
+
+pub fn get() -> Result<f32> {
+    unsafe { get_endpoint().unwrap().GetMasterVolumeLevelScalar() }
+}
+pub fn set(vol: f32) -> Result<()> {
+    unsafe {
+        get_endpoint().unwrap().SetMasterVolumeLevelScalar(vol, core::ptr::null())
+    }
+}
